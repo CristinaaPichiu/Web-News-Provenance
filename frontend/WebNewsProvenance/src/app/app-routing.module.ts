122b6cb3c9ef8b0ee4ef063a4cfe339bba32d364
Cristina Pichiu <cristinapichiu38@gmail.com>
2025-01-24 23:56:24 +0200

    added routing, sidebar component and refining styles

--- frontend/WebNewsProvenance/src/app/app-routing.module.ts
+++ frontend/WebNewsProvenance/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'register',
+    component: RegisterComponent
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path:'sidebar',
+    component: SidebarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
