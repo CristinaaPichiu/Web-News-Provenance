23d2dc7aed1c9510bdff3d35db2a014562a5380e
Miruna Curduman <curduman.miruna@gmail.com>
2025-01-30 17:12:16 +0200

    improved responsiveness

--- frontend/WebNewsProvenance/src/app/components/register/register.component.ts
+++ frontend/WebNewsProvenance/src/app/components/register/register.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, Inject, Renderer2, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-register',
@@ -13,14 +12,16 @@ import { AuthService } from 'src/app/services/auth.service';
 export class RegisterComponent {
   hidePassword = true;
   hideConfirmPassword = true;
-  passwordPattern = '^(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$'; 
+  passwordPattern = '^(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$';
   registerForm!: FormGroup;
   errorMessage: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService, // Injectează AuthService
-    private router: Router
+    private router: Router,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit(): void {
@@ -31,13 +32,14 @@ export class RegisterComponent {
       password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]],
       confirmPassword: ['', [Validators.required]]
     });
-    
+    this.addJsonLd();
+
   }
 
   checkPasswords(group: FormGroup): any {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
-    console.log("Password Check: ", password, confirmPassword); 
+    console.log("Password Check: ", password, confirmPassword);
     return password === confirmPassword ? null : { notSame: true };
   }
 
@@ -65,5 +67,64 @@ export class RegisterComponent {
     } else {
       this.errorMessage = 'Please check your registration details and try again.';
     }
+  }
+
+  addJsonLd() {
+    const script = this.renderer.createElement('script');
+    script.type = 'application/ld+json';
+    script.text = JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "WebPage",
+      "name": "Login Page",
+      "description": "Login to your account",
+      "image": { "@type": "ImageObject", "url": "https://github.com/CristinaaPichiu/Web-News-Provenance/blob/main/frontend/WebNewsProvenance/src/assets/registerBun.png" },
+      "author": [
+        {
+          "@type": "Person",
+          "name": "Pichiu Cristina-Cătălina",
+          "url": "https://github.com/CristinaaPichiu"
+        },
+        {
+          "@type": "Person",
+          "name": "Curduman Miruna-Diana",
+          "url": "https://github.com/curduman-miruna/"
+        }
+      ],
+      "hasPart": {
+        "@type": "WebPageElement",
+        "name": "Register Form",
+        "cssSelector": ".register-container",
+        "description": "A registration form that allows users to create a new account."
+      },
+      "potentialAction": {
+        "@type": "RegisterAction",
+        "target": "https://yourcompany.com/api/register",
+        "name": "User Registration",
+        "description": "Allows a new user to create an account.",
+        "input": [
+          {
+            "@type": "PropertyValue",
+            "name": "First Name",
+            "valueRequired": true
+          },
+          {
+            "@type": "PropertyValue",
+            "name": "Last Name",
+            "valueRequired": true
+          },
+          {
+            "@type": "PropertyValue",
+            "name": "Email",
+            "valueRequired": true
+          },
+          {
+            "@type": "PropertyValue",
+            "name": "Password",
+            "valueRequired": true
+          }
+        ]
+      }
+    });
+    this.renderer.appendChild(this.document.head, script);
   }
 }
